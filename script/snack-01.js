b5113c682649127eba93160f82f8c7173ffce0bd
Rserli <riccardoserli.95@gmail.com>
2025-05-16 21:30:29 +0200

    JSnack1-Variables

--- script/snack-01.js
+++ script/snack-01.js
@@ -2,3 +2,21 @@ console.log(`
     --------------------------------
         JSnack-01
     `)
+
+// VARIABILI
+const automobili = [
+    { marca: "Fiat", modello: "Panda", alimentazione: "Benzina" },
+    { marca: "Volkswagen", modello: "Golf", alimentazione: "Diesel" },
+    { marca: "Tesla", modello: "Model 3", alimentazione: "Elettrica" },
+    { marca: "Toyota", modello: "Corolla", alimentazione: "GPL" },
+    { marca: "Ford", modello: "Focus", alimentazione: "Benzina" },
+    { marca: "BMW", modello: "X3", alimentazione: "Diesel" },
+    { marca: "Renault", modello: "Clio", alimentazione: "GPL" },
+    { marca: "Audi", modello: "A4", alimentazione: "Diesel" },
+    { marca: "Peugeot", modello: "208", alimentazione: "Benzina" },
+    { marca: "Hyundai", modello: "Ioniq", alimentazione: "Elettrica" }
+];
+
+// PROCESSO
+
+// OUTPUT
