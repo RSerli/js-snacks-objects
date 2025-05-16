61eeb01fee41a85de4ae5fd229f2bf0baf4d6722
Rserli <riccardoserli.95@gmail.com>
2025-05-16 21:39:02 +0200

    JSnack1-For Loop

--- script/snack-01.js
+++ script/snack-01.js
@@ -4,6 +4,7 @@ console.log(`
     `)
 
 // VARIABILI
+// array insieme di automobili
 const automobili = [
     { marca: "Fiat", modello: "Panda", alimentazione: "Benzina" },
     { marca: "Volkswagen", modello: "Golf", alimentazione: "Diesel" },
@@ -16,7 +17,16 @@ const automobili = [
     { marca: "Peugeot", modello: "208", alimentazione: "Benzina" },
     { marca: "Hyundai", modello: "Ioniq", alimentazione: "Elettrica" }
 ];
+// array auto con alimentazione a benzina
+let AutoBenzina = []
+// array auto con alimentazione a diesel
+let AutoDiesel = []
+// array auto con gli atri tipi di alimentazione
+let AutoAltro = []
 
 // PROCESSO
-
+for (let i = 0; i < automobili.length; i++) {
+    const CurrentElement = automobili[i];
+    console.log(CurrentElement)
+}
 // OUTPUT
