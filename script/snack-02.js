730ef805fe881443a56ff4dcc0adf8680c2ca0fc
Rserli <riccardoserli.95@gmail.com>
2025-05-16 22:05:02 +0200

    JSnack2-Varaibles

--- script/snack-02.js
+++ script/snack-02.js
@@ -4,6 +4,24 @@ console.log(`
     `)
 
 // VARIABILI
+const ElencoNomi = [
+    "mario",         // tutto minuscolo
+    "LUCA",          // tutto maiuscolo
+    "giulia",        // tutto minuscolo
+    "ANNA",          // tutto maiuscolo
+    "Marco",         // corretto
+    "aLessandra",    // prima minuscola, resto misto
+    "PAOLO",         // tutto maiuscolo
+    "francesca",     // tutto minuscolo
+    "EDOARDO",       // tutto maiuscolo
+    "federica",      // tutto minuscolo
+    "GIORGIO",       // tutto maiuscolo
+    "nIcole",        // prima minuscola, resto misto
+    "Elena",         // corretto
+    "giovanni",      // tutto minuscolo
+    "SIMONE"         // tutto maiuscolo
+];
+let ElencoNomiFormattati = []
 
 // PROCESSO
 
