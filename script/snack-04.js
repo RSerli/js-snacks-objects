5d3ac5414b769a52706a96364edf835bcae2fdb2
Rserli <riccardoserli.95@gmail.com>
2025-05-16 23:27:00 +0200

    JSnack4-Begin

--- script/snack-04.js
+++ script/snack-04.js
@@ -4,7 +4,22 @@ console.log(`
     `)
 
 // VARIABILI
+const ElencoPersone = [
+    { nome: "Carla", cognome: "Ferrari", anni: 18 },
+    { nome: "Federico", cognome: "Giovanardi", anni: 35 },
+    { nome: "Giovanni", cognome: "Tardini", anni: 54 },
+    { nome: "Francesca", cognome: "Chiari", anni: 16 },
+    { nome: "Duilio", cognome: "Carboni", anni: 30 },
+    { nome: "Francesco", cognome: "Del Rio", anni: 48 },
+    { nome: "Eleonora", cognome: "Grandi", anni: 23 },
+    { nome: "Samuele", cognome: "Gregori", anni: 15 },
+    { nome: "Chiara", cognome: "Tondi", anni: 16 },
+    { nome: "Agnese", cognome: "De Santis", anni: 34 },
+    { nome: "Enrico", cognome: "Spaggiari", anni: 68 },
+    { nome: "Caterina", cognome: "Foschi", anni: 28 }
+]
 
+let ElencoIndicazioneGuidaAbilitata = []
 // PROCESSO
 
 // OUTPUT
