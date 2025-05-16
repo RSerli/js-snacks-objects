16b2f58351ba994d77e5164e9aed7f47072db08f
Rserli <riccardoserli.95@gmail.com>
2025-05-16 23:04:52 +0200

    JSnack3-Begin

--- script/snack-03.js
+++ script/snack-03.js
@@ -2,3 +2,20 @@ console.log(`
     --------------------------------
         JSnack-03
     `)
+
+// VARIABILI
+const animali = [
+    { nome: "Leone", famiglia: "Felidi", classe: "Mammiferi" },
+    { nome: "Aquila", famiglia: "Accipitridi", classe: "Uccelli" },
+    { nome: "Cane", famiglia: "Canidi", classe: "Mammiferi" },
+    { nome: "Coccodrillo", famiglia: "Crocodilidi", classe: "Rettili" },
+    { nome: "Gatto", famiglia: "Felidi", classe: "Mammiferi" },
+    { nome: "Squalo bianco", famiglia: "Lamnidi", classe: "Pesci" },
+    { nome: "Elefante", famiglia: "Elefantidi", classe: "Mammiferi" },
+    { nome: "Pinguino", famiglia: "Sfeniscidi", classe: "Uccelli" },
+    { nome: "Serpente", famiglia: "Colubridi", classe: "Rettili" },
+    { nome: "Gufo", famiglia: "Strigidi", classe: "Uccelli" }
+];
+// PROCESSO
+
+// OUTPUT
