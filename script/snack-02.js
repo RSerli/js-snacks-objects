465cf4004bf04f51d9d4a1caf9696c8af099bc85
Rserli <riccardoserli.95@gmail.com>
2025-05-16 22:52:49 +0200

    JSnack2-Begin Uppercase

--- script/snack-02.js
+++ script/snack-02.js
@@ -12,11 +12,11 @@ const ElencoNomi = [
     "Marco",         // corretto
     "aLessandra",    // prima minuscola, resto misto
     "PAOLO",         // tutto maiuscolo
-    "francesca",     // tutto minuscolo
+    "  francesca",   // tutto minuscolo, spazio all'inizio
     "EDOARDO",       // tutto maiuscolo
     "federica",      // tutto minuscolo
     "GIORGIO",       // tutto maiuscolo
-    "nIcole",        // prima minuscola, resto misto
+    "  nIcole",      // prima minuscola, resto misto, spazio all'inizio
     "Elena",         // corretto
     "giovanni",      // tutto minuscolo
     "SIMONE"         // tutto maiuscolo
@@ -27,9 +27,17 @@ let ElencoNomiFormattati = []
 for (let i = 0; i < ElencoNomi.length; i++) {
     const CurrentName = ElencoNomi[i];
     // console.log(CurrentName)
+    // Eleminazione spazio bianco all'inizio della stringa 
+    const CurrentNameNoSpace = CurrentName.trimStart()
     // set LowerCase tutta la stringa in CurrentName
-    const CurrentName_LowerCase = CurrentName.toLowerCase()
-    ElencoNomiFormattati.push(CurrentName_LowerCase)
+    const CurrentName_LowerCase = CurrentNameNoSpace.toLowerCase()
+    console.log(CurrentName_LowerCase)
+    // set FirstLetter to UpperCase
+    const CurrentName_FirsLetterUp = CurrentName_LowerCase[0].toUpperCase()
+    console.log(CurrentName_FirsLetterUp)
+    const CurrentName_RestString = CurrentName_LowerCase.slice(1)
+    console.log(CurrentName_RestString)
+    ElencoNomiFormattati.push(CurrentName_FirsLetterUp)
 }
 // OUTPUT
-console.log(ElencoNomiFormattati)
+// console.log(ElencoNomiFormattati)
